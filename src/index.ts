268462a9769925c6cb36a43f91df874ee79cb0f0
Kasun Dhananjaya <125995451+Kasundananjaya297@users.noreply.github.com>
2025-04-13 07:20:06 +0530

    fk

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+/** @format */
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL || "";
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("Server is running");
+});
+app.get("/info", async (req, res) => {
+  res.send({
+    PORT: PORT,
+    NODE_ENV: process.env.ENV,
+    DATABASE_URL: process.env.DB_URL,
+  });
+});
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log(`🌎 | App Started on  http://localhost:${PORT}`);
+  })
+  .catch((error: any) => {
+    console.log("Error occurred while connecting to database", error);
+  });
+
+app
+  .listen(PORT, () => {
+    console.log(
+      `Server is Successfully Running, and App is listening on port ${PORT}`,
+    );
+  })
+  .on("error", (error: any) => {
+    console.log("Error occurred, server can't start", error);
+  });
